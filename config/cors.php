d387bf18eaf26cd041529092fdbcfb99be75a9b6
joel.daho <joel.daho@asnumeric.com>
2025-05-06 00:18:29 +0000

    Cors method updated & bugs fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://gest-front.vercel.app/'],
+    'allowed_origins' => ['https://gest-front.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
